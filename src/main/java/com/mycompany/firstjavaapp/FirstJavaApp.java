9451ea918ed32cc6b64d7d007e1350a46e5570a3
LAPTOP-N175LAT4\ASUS <nguyenkhanhtrang24042004@gmail.com>
2024-02-01 23:14:25 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/firstjavaapp/FirstJavaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstjavaapp;
+
+/**
+ *
+ * @author ASUS
+ */
+public class FirstJavaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
